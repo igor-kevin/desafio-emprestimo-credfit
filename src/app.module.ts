be3bcd65d5ff8d1817706673ba2de929378329e9
Igor Kevin <igor_kevin@hotmail.com>
2024-05-14 13:56:13 -0300

    Conexão no banco de dados "desafio_credfit" no postgres, além da instalação do configmodule com a CLI: "npm --install @nestjs/config"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RepresentantesModule } from './representantes/representantes.module';
 import { FuncionariosModule } from './funcionarios/funcionarios.module';
 import { EmprestimosModule } from './emprestimos/emprestimos.module';
+import { join } from 'path';
 
 @Module({
-  imports: [RepresentantesModule, FuncionariosModule, EmprestimosModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        synchronize: true,  // NÃO USA EM PRODUÇÃO
+      })
+    }),
+    RepresentantesModule,
+    FuncionariosModule,
+    EmprestimosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
