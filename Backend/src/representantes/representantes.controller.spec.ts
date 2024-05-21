84fead826d6e16812213dc46ca205c23a6311101
Igor Kevin <igor_kevin@hotmail.com>
2024-05-21 01:35:01 -0300

    Teste Unitario do Representantes.controller ok

--- Backend/src/representantes/representantes.controller.spec.ts
+++ Backend/src/representantes/representantes.controller.spec.ts
@@ -7,6 +7,7 @@ import { Repository } from 'typeorm';
 import { CreateRepresentanteDto } from './dto/create-representante.dto';
 import { Funcionario } from 'src/funcionarios/entities/funcionario.entity';
 import { ConflictException, NotFoundException } from '@nestjs/common';
+import { UpdateRepresentanteDto } from './dto/update-representante.dto';
 
 describe('RepresentantesController', () => {
   let controller: RepresentantesController;
@@ -47,10 +48,23 @@ describe('RepresentantesController', () => {
       } else {
         throw new NotFoundException(`Não achou o representante de id #${id}`);
       }
-    })
-
-
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        ...dto,
+        representante_id: id,
+        funcionario: new Funcionario()
+      };
+    }),
+    remove: jest.fn((id: number) => {
+      if (id === 1) {
+        return;
+      } else {
+        throw new NotFoundException(`Não achou o representante de id #${id}`);
+      }
+    }),
   }
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -129,7 +143,7 @@ describe('RepresentantesController', () => {
     })
   }
   );
-  describe('Teste do FindAll', () => {
+  describe('Teste do FindOne', () => {
     it('Tem que retornar um representante dado seu id', async () => {
       const respostaEsperada = {
         representante_id: 1,
@@ -141,5 +155,79 @@ describe('RepresentantesController', () => {
       expect(result).toEqual(respostaEsperada);
       expect(service.findOne).toHaveBeenCalledWith(1);
     })
+
+    it('Tem que retornar um erro por não encontrar', async () => {
+      (mockRepresentantesService.findOne as jest.Mock).mockRejectedValueOnce(
+        new NotFoundException(`Não achou o representante de id #${1}`),
+      );
+
+      await expect(controller.findOne('1')).rejects.toThrow(NotFoundException);
+
+      expect(service.findOne).toHaveBeenCalledWith(1);
+    })
+  })
+
+  describe('Testes do update', () => {
+    it('Deve atualizar um representante', async () => {
+      const updateDto: UpdateRepresentanteDto = {
+        representante_cnpj: 2,
+        representante_razao_social: 'Empresa atualizada'
+      };
+      const respostaEsperada = {
+        representante_id: 1,
+        representante_cnpj: 2,
+        representante_razao_social: 'Empresa atualizada',
+        funcionario: expect.any(Funcionario)
+      };
+
+      const result = await controller.update('1', updateDto);
+
+      expect(result).toEqual(respostaEsperada);
+      expect(service.update).toHaveBeenCalledWith(1, updateDto);
+    });
+
+    it('Deve lançar um NotFoundException se o representante não for encontrado', async () => {
+      const updateDto: UpdateRepresentanteDto = {
+        representante_cnpj: 2,
+        representante_razao_social: 'Empresa atualizada'
+      };
+
+      mockRepresentantesService.update.mockRejectedValueOnce(new NotFoundException(`Não achou o representante de id #${1}`));
+
+      await expect(controller.update('1', updateDto)).rejects.toThrow(NotFoundException);
+
+      expect(service.update).toHaveBeenCalledWith(1, updateDto);
+    });
+
+    it('Deve lançar um ConflictException se houver um conflito de dados únicos', async () => {
+      const updateDto: UpdateRepresentanteDto = {
+        representante_cnpj: 2,
+        representante_razao_social: 'Empresa atualizada'
+      };
+
+      mockRepresentantesService.update.mockRejectedValueOnce(new ConflictException('Já existe um representante com algum desses dados que eram para ser unique.'));
+
+      await expect(controller.update('1', updateDto)).rejects.toThrow(ConflictException);
+
+      expect(service.update).toHaveBeenCalledWith(1, updateDto);
+    });
   });
+
+  describe('Testes do delete', () => {
+    it('Deve deletar um representante dado seu id', () => {
+      controller.remove('1');
+      expect(service.remove).toHaveBeenCalledWith(1);
+    });
+
+    it('Deve lançar um NotFoundException se o representante não for encontrado', () => {
+      (mockRepresentantesService.remove as jest.Mock).mockRejectedValueOnce(
+        new NotFoundException(`Não achou o representante de id #${2}`),
+      );
+
+      expect(controller.remove('2')).rejects.toThrow(NotFoundException);
+      expect(service.remove).toHaveBeenCalledWith(2);
+    });
+  });
+
 });
+
